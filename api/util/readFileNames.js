58671b5eda1b67ffdc99676ccec5f5fd27489984
Mark <ratrateroo@gmail.com>
2022-02-19 23:34:21 +0800

    added fine name reader for public images folder

---
+++ api/util/readFileNames.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const EventEmitter = require('events').EventEmitter;
+const filesEE = new EventEmitter();
+const myfiles = [];
+const folderName = './public/images';
+
+// this event will be called when all files have been added to myfiles
+filesEE.on('files_ready', function () {
+	console.dir(myfiles);
+});
+
+console.log('length to zero');
+const readFileNames = () => {
+	// read all files from current directory
+	fs.readdir(folderName, function (err, files) {
+		myfiles.length = 0;
+		if (err) throw err;
+		files.forEach(function (file) {
+			myfiles.push(file);
+		});
+		filesEE.emit('files_ready'); // trigger files_ready event
+	});
+	console.log('returning files');
+	return myfiles;
+};
+module.exports = readFileNames;
