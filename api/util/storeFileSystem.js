d49570bb67bf9d35bb5ff61b2626349a31ad4c47
Mark <ratrateroo@gmail.com>
2022-02-21 23:21:13 +0800

    added store file system

---
+++ api/util/storeFileSystem.js
@@ -0,0 +1,58 @@
+const shortId = require('shortid');
+const path = require('path');
+const { createWriteStream, unlink } = require('fs');
+
+const generateRandomString = require('./gernerateRamdomString');
+
+//const files = require('./files');
+//root directory __dirname
+//file upload directory /public/images
+
+const storeFileSystem = async ({ stream, filename, mimetype, encoding }) => {
+	//const id = shortId.generate();
+
+	//const storedFileName = `${id}-${filename}`;
+
+	//new storedFileName
+	//get extension name and file name
+	const { ext, name } = path.parse(filename);
+
+	//generate new storedFileName
+
+	const storedFileName = generateRandomString(12) + ext;
+
+	const newurl = new URL(path.join(__dirname, './public/images'));
+
+	//storedFileUrl - file upload directory + filename
+	const storedFileUrl = path.join(
+		__dirname,
+		`./public/images/${storedFileName}`
+	);
+
+	//Store the file in the filesystem.
+	await new Promise((resolve, reject) => {
+		//Create a stream to which the upload will be written.
+		const writeStream = createWriteStream(storedFileUrl);
+
+		//When the upload is fully written, resolve the promise.
+		writeStream.on('finish', resolve);
+
+		//If there's an error writing the file,
+		//remove the partially written file
+		//and reject the promise.
+		writeStream.on('error', (error) => {
+			unlink(storedFileUrl, () => {
+				reject(error);
+			});
+		});
+
+		stream.on('error', (error) => writeStream.destroy(error));
+		stream.pipe(writeStream);
+	});
+
+	files.push(storedFileName);
+
+	return { filename: storedFileName, mimetype: mimetype, encoding: encoding };
+};
+
+module.exports = storeFileSystem;
