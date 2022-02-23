2a25539e4102341beff7331422749f8c13e30f9b
Mark <ratrateroo@gmail.com>
2022-02-23 23:35:09 +0800

    added create directory to the api

---
+++ api/util/createDirectory.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const { mkdirSync, existsSync, stat } = require('fs');
+
+const createDirectory = (foldername) => {
+	existsSync(path.join(__dirname, `./public/${foldername}`)) ||
+		mkdirSync(path.join(__dirname, `./public/${foldername}`));
+
+	// Getting information for a directory
+
+	stat(path.join(__dirname, `./public/${foldername}`), (error, stats) => {
+		if (error) {
+			console.log(error);
+		} else {
+			console.log(
+				`Stats object for: ${path.join(
+					__dirname,
+					`./public/${foldername}`
+				)}`
+			);
+			//console.log(stats);
+			// Using methods of the Stats object
+			console.log('Path is file:', stats.isFile());
+			console.log('Path is directory:', stats.isDirectory());
+		}
+	});
+};
+module.exports = createDirectory;
