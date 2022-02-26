e7e1903ceea03cf57a0ab5ded67fd965fb08508a
Mark <ratrateroo@gmail.com>
2022-02-26 22:51:15 +0800

    added layout to the app

---
+++ client/src/shared/components/Layout/Layout.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+// import MainNavigation from '../Navigation/MainNavigation';
+// import AuthProvider from '../../auth/AuthProvider';
+import { Outlet } from 'react-router';
+
+const Layout = (props) => {
+	return (
+		// <AuthProvider>
+		<Fragment>
+			{/* <MainNavigation /> */}
+			<Outlet />
+		</Fragment>
+		// </AuthProvider>
+	);
+};
+
+export default Layout;
