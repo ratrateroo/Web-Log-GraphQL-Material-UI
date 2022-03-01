587828105a825618a9af822782c77a55ecf7c374
Mark <ratrateroo@gmail.com>
2022-03-01 23:44:45 +0800

    added color mode context to the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
-import { Fragment } from 'react';
+import { Fragment, useContext } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 import Layout from './shared/components/Layout/Layout';
 import SignUp from './user/pages/SignUp';
 
+import { ColorModeContext } from './context/ColorModeContext';
+
 const App = () => {
+	const colorMode = useContext(ColorModeContext);
 	return (
 		<Fragment>
 			<Routes>
