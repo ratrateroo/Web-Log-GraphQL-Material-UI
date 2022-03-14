d783c07df8ff2b25b9b02cda962757d0840ce493
Mark <ratrateroo@gmail.com>
2022-03-15 02:17:45 +0800

    started building user profile page ui

--- client/src/user/components/UserProfileInfo.js
+++ client/src/user/components/UserProfileInfo.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 
 import { gql, useQuery } from '@apollo/client';
 import { useTheme } from '@mui/material';
+import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -53,24 +54,24 @@ const UserProfileInfo = () => {
 					maxWidth: { xs: 'xs', sm: 'sm', md: 'md', lg: 'lg', xl: 'xl' },
 				}}>
 				<CssBaseline />
+				{loading ? (
+					<Typography variant="h5" component="h5" color="secondary">
+						Loading user...
+					</Typography>
+				) : (
+					<Typography variant="h5" component="h5" color="secondary">
+						{data.user.firstname + ' ' + data.user.middlename + ' ' + data.user.lastname}
+					</Typography>
+				)}
 
-				<Typography variant="h5" component="h5" color="secondary">
-					Users List
-				</Typography>
+				{error && (
+					<Typography variant="h5" component="h5" color="secondary">
+						Some error occured...
+					</Typography>
+				)}
 
 				<Box sx={{ flexGrow: 1, mt: 2, p: 2 }}>
 					<Grid container spacing={2}>
-						{error && (
-							<Grid>
-								<Typography
-									gutterBottom
-									variant="h6"
-									component="h6"
-									color={theme.palette.error.main}>
-									Some error occured...
-								</Typography>
-							</Grid>
-						)}
 						{loading ? (
 							<Grid>
 								<Typography
@@ -78,18 +79,20 @@ const UserProfileInfo = () => {
 									variant="h6"
 									component="h6"
 									color={theme.palette.info.main}>
-									Loading users...
+									Loading user...
 								</Typography>
 							</Grid>
 						) : (
 							<Grid>
-								<Typography
-									gutterBottom
-									variant="h6"
-									component="h6"
-									color={theme.palette.info.main}>
-									Data
-								</Typography>
+								<Avatar
+									alt="Profile Image"
+									src={
+										data.user.profileimage === 'defaultimage'
+											? 'http://localhost:8000/freefiles/images/user_image.png'
+											: `http://localhost:8000/freefiles/images/${data.user.profileimage}`
+									}
+									sx={{ width: 56, height: 56 }}
+								/>
 							</Grid>
 						)}
 					</Grid>
