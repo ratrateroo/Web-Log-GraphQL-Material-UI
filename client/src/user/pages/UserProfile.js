910d5b6820b9c062abd5419716411fc8011ede96
Mark <ratrateroo@gmail.com>
2022-03-28 22:55:06 +0800

    updated user profile page

--- client/src/user/pages/UserProfile.js
+++ client/src/user/pages/UserProfile.js
@@ -3,7 +3,7 @@ import React from 'react';
 import UserProfileInfo from '../components/UserProfileInfo';
 
 const UserProfile = () => {
-	return <UserProfileInfo id="622c7e8e949a05e24be5cf94" />;
+	return <UserProfileInfo />;
 };
 
 export default UserProfile;
