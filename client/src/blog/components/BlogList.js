4035b58932e6845f85c7160d6623de17926068aa
Mark <ratrateroo@gmail.com>
2022-03-30 23:21:18 +0800

    added blogs page and blog list component

---
+++ client/src/blog/components/BlogList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BlogList = () => {
+	return <h1>Blog List</h1>;
+};
+
+export default BlogList;
---
+++ client/src/blog/pages/Blogs.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import BlogList from '../components/BlogList';
+
+const Blogs = () => {
+	return <BlogList />;
+};
+
+export default Blogs;
