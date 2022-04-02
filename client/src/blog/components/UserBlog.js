6d3026509f65c78499cedebc4ca0fe5b1eebfe19
Mark <ratrateroo@gmail.com>
2022-04-02 12:32:42 +0800

    added blog page and user blog component

---
+++ client/src/blog/components/UserBlog.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const UserBlog = () => {
+	return <h1>User Blog</h1>;
+};
+
+export default UserBlog;
---
+++ client/src/blog/pages/Blog.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import UserBlog from '../components/UserBlog';
+
+const Blog = () => {
+	return <UserBlog />;
+};
+
+export default Blog;
