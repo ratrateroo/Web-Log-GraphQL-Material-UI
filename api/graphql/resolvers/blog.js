12c22838c5d736b02ac21f1a1bd4fee3af6ea42c
Mark <ratrateroo@gmail.com>
2022-04-10 21:51:57 +0800

    updated blog resolver

--- api/graphql/resolvers/blog.js
+++ api/graphql/resolvers/blog.js
@@ -11,44 +11,22 @@ const blogResolvers = {
 	createBlog: async (args) => {
 		console.log(args);
 		let status;
-		await User.findOne()
-			.or([{ username: args.userInput.username }, { email: args.userInput.email }])
-			.then((result) => {
-				console.log(result);
-				{
-					result.username === args.userInput.username
-						? (status = 'username')
-						: (status = 'email');
-				}
-			})
-			.catch((err) => console.log(err));
 
-		if (status === 'username') {
-			throw new Error('Username exists already.');
-		}
-
-		if (status === 'email') {
-			throw new Error('Email already taken.');
-		}
-
-		const hashedPassword = await bcrypt.hash(args.userInput.password, 12);
-
-		const user = new User({
-			username: args.userInput.username,
-			email: args.userInput.email,
-			password: hashedPassword,
-			firstname: args.userInput.firstname,
-			middlename: args.userInput.middlename,
-			lastname: args.userInput.lastname,
-			profileimage: 'defaultimage',
+		const blog = new Blog({
+			title: args.blogInput.username,
+			content: args.blogInput.email,
+			likes: 0,
+			comments: [''],
 		});
-		const result = await user.save();
+		const result = await blog.save();
 
-		const token = jwt.sign({ userId: user.id, email: user.email }, process.env.TOKEN_SECRET_KEY, {
-			expiresIn: '1h',
-		});
-
-		return { userId: user.id, token: token, tokenExpiration: 1 };
+		return {
+			blogId: blog.id,
+			title: blog.title,
+			content: blog.content,
+			likes: blog.likes,
+			comments: blog.comments,
+		};
 	},
 };
 module.exports = userResolvers;
