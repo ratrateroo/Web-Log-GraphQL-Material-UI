f54b8ffc599d38713945a6c5f64a81abafbdb497
Mark <ratrateroo@gmail.com>
2022-04-28 23:56:04 +0800

    added list for blog list page

--- client/src/blog/components/BlogsList.js
+++ client/src/blog/components/BlogsList.js
@@ -1,11 +1,19 @@
 import React, { Fragment, useState, useEffect } from 'react';
 
 import { gql, useLazyQuery } from '@apollo/client';
+import DeleteIcon from '@mui/icons-material/Delete';
+import FolderIcon from '@mui/icons-material/Folder';
 import { useTheme } from '@mui/material';
+import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemText from '@mui/material/ListItemText';
 import Typography from '@mui/material/Typography';
 
 const BLOGS_QUERY = gql`
@@ -56,6 +64,30 @@ const BlogsList = () => {
 				<Typography variant="h5" component="h5" color="secondary">
 					Blogs List
 				</Typography>
+				<Box>
+					<Grid item xs={12} md={6}>
+						<List>
+							{loadedBlogs.map((blog) => {
+								return (
+									<ListItem
+										key={blog.blogId}
+										secondaryAction={
+											<IconButton edge="end" aria-label="delete">
+												<DeleteIcon />
+											</IconButton>
+										}>
+										<ListItemAvatar>
+											<Avatar>
+												<FolderIcon />
+											</Avatar>
+										</ListItemAvatar>
+										<ListItemText primary={blog.title} secondary={blog.author.username} />
+									</ListItem>
+								);
+							})}
+						</List>
+					</Grid>
+				</Box>
 			</Container>
 		</Fragment>
 	);
