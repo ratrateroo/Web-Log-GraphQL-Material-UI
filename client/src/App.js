5d3bd063a8ce15966d9bd7161cf3b9b2810b14e8
Mark <ratrateroo@gmail.com>
2022-05-04 00:12:18 +0800

    added blog page to the app

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import React, { Fragment, useContext, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import RequireAuth from './auth/RequireAuth';
+import Blog from './blog/pages/Blog';
 import Blogs from './blog/pages/Blogs';
 import CreateBlog from './blog/pages/CreateBlog';
 import { ColorModeContext } from './context/ColorModeContext';
@@ -35,6 +36,7 @@ const App = () => {
 
 					<Route path="/signup" element={<UserSignUp />} />
 					<Route path="/login" element={<UserLogIn />} />
+					<Route path="/blog" element={<Blog />} />
 					<Route path="/blogs" element={<Blogs />} />
 					<Route
 						path="/users"
--- client/src/blog/components/UserBlog.js
+++ client/src/blog/components/UserBlog.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
