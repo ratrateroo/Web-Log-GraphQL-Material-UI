95a51ac0a9f22f5d3d44a71a0acf19e32624f770
Mark <ratrateroo@gmail.com>
2022-05-03 23:49:11 +0800

    added imports for user blog

--- client/src/blog/components/UserBlog.js
+++ client/src/blog/components/UserBlog.js
@@ -1,5 +1,18 @@
 import React from 'react';
 
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Grid from '@mui/material/Grid';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Typography from '@mui/material/Typography';
 const UserBlog = () => {
 	return <h1>User Blog</h1>;
 };
